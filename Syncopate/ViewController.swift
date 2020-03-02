efcff3928929b65dc8e1c60d8cc82ed6b6bedfec
Shafer <shafer.hess01@gmail.com>
2020-03-02 11:18:46 -0500

    Initial Commit

---
+++ Syncopate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Syncopate
+//
+//  Created by Shafer Hess on 3/2/20.
+//  Copyright © 2020 Syncopate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
