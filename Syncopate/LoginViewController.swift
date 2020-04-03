bf5ac6e644ec944ff0c7b5464591e367d89d8872
Shafer <shafer.hess01@gmail.com>
2020-04-02 21:14:31 -0400

    Added login outlets

---
+++ Syncopate/LoginViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewController.swift
+//  Syncopate
+//
+//  Created by Shafer Hess on 4/2/20.
+//  Copyright © 2020 Syncopate. All rights reserved.
+//
+
+import Alamofire
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var registerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
