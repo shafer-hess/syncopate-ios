a4714d9180708ac2b90a0f24bbb15735e2d61034
Shafer <shafer.hess01@gmail.com>
2020-04-07 17:21:53 -0400

    Added Assets for future UI work. Added tab bar images. Created and set up ViewControllers for Friends and Profile Tab

---
+++ Syncopate/FriendsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FriendsViewController.swift
+//  Syncopate
+//
+//  Created by Shafer Hess on 4/7/20.
+//  Copyright © 2020 Syncopate. All rights reserved.
+//
+
+import UIKit
+
+class FriendsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
