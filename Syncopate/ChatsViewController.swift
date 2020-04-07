40031c490ba1255a4f1ef023ebc95ab6c427dc90
Shafer <shafer.hess01@gmail.com>
2020-04-06 20:07:12 -0400

    Added refresh controller and stored groups in global variable

--- Syncopate/ChatsViewController.swift
+++ Syncopate/ChatsViewController.swift
@@ -15,7 +15,9 @@ class ChatsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBOutlet weak var logoutButton: UIBarButtonItem!
     @IBOutlet weak var chatsTableView: UITableView!
     
-    var Groups: NSArray = []
+    let myRefreshController = UIRefreshControl()
+    
+    var groups: NSArray = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,25 +25,32 @@ class ChatsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         // Do any additional setup after loading the view.
         self.chatsTableView.delegate = self
         self.chatsTableView.dataSource = self
+        
+        myRefreshController.addTarget(self, action: #selector(getUserGroups), for: .valueChanged)
+        self.chatsTableView.refreshControl = myRefreshController
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         getUserGroups()
+        self.chatsTableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return groups.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = chatsTableView.dequeueReusableCell(withIdentifier: "ChatsCell") as! ChatsCell
         
+        
+        
+        
         return cell
     }
     
     // Get UserGroups Request
-    func getUserGroups() {
+    @objc func getUserGroups() {
         // UserGroups Endpoint
         let url = "http://18.219.112.140:8000/api/v1/get-user-group/"
         
@@ -51,18 +60,9 @@ class ChatsViewController: UIViewController, UITableViewDelegate, UITableViewDat
             switch response.result {
                 case .success(let value):
                     if let data = value as? NSArray {
-                        
-                        // Getting Info From Response
-                        let dict = data[0] as! NSDictionary
-                        let user_entry = dict["users"] as! NSArray
-                        for user in user_entry {
-                            let user_info = user as! NSDictionary
-                            print(user_info["user__first_name"] as! String)
-                        }
-                        
-                        
-                        // self.groups = data
-                        // print(self.groups)
+                        self.groups = data
+                        self.chatsTableView.reloadData()
+                        self.myRefreshController.endRefreshing()
                     }
                 
                 case .failure(let error):
