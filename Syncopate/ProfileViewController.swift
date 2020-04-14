08e427f8bc1c50c5a3546fcd608228c0103772f9
Emily ou <emilyou34@gmail.com>
2020-04-14 11:57:10 -0400

    Added UI, profile picture, name, and email to profile view controller

--- Syncopate/ProfileViewController.swift
+++ Syncopate/ProfileViewController.swift
@@ -7,16 +7,54 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
 
 class ProfileViewController: UIViewController {
-
+    // Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        getUserInfo()
     }
     
+    func getUserInfo() {
+        // user info endpoint
+        let url = "http://18.219.112.140:8000/api/v1/identify/"
 
+        // HTTP Request
+        AF.request(url, method: .get, encoding: JSONEncoding.default).responseJSON { (response) in
+            switch response.result {
+                case .success(let value):
+                    if let data = value as? [String : Any] {
+                        // Get name
+                        let first_name = data["first_name"] as? String
+                        let last_name = data["last_name"] as? String
+                        let full_name = first_name! + " " + last_name!
+                        self.nameLabel.text = full_name
+                        
+                        // Get email and profile pic
+                        self.emailLabel.text = data["email"] as? String
+                        let picURL = (data["profile_pic_url"] as? String)!
+                        let imageURL = URL(string: picURL)!
+                        self.profileImage.af.setImage(withURL: imageURL)
+                        
+                        // Make the profile pic circular
+                        self.profileImage.layer.masksToBounds = false
+                        self.profileImage.layer.cornerRadius = self.profileImage.frame.height / 2
+                        self.profileImage.clipsToBounds = true
+                    }
+                
+                case .failure(let error):
+                    print(error.localizedDescription)
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
