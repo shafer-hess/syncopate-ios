e796b2a4bcdcca3ad42aca86fa740b514eb845c1
Emily ou <emilyou34@gmail.com>
2020-04-19 19:40:58 -0400

    Added create group request/function

--- Syncopate/CreateGroupViewController.swift
+++ Syncopate/CreateGroupViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
 
 class CreateGroupViewController: UIViewController, UITextViewDelegate {
     // Outlets
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var dmButton: UIButton!
+    var checked: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,10 +33,53 @@ class CreateGroupViewController: UIViewController, UITextViewDelegate {
         descriptionTextView.layer.cornerRadius = 10.0
     }
     
+    @IBAction func onSubmit(_ sender: Any) {
+        createGroup()
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        self.descriptionTextView.endEditing(true)
+        self.nameField.endEditing(true)
+    }
+ 
     @IBAction func cancelButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    func setChecked(_ isChecked: Bool) {
+        checked = isChecked
+        if checked {
+            dmButton.setImage(UIImage(named: "checkmark.square"), for: UIControl.State.normal)
+        } else {
+            dmButton.setImage(UIImage(named: "square"), for: UIControl.State.normal)
+        }
+    }
+    
+    func createGroup() {
+        // Create group endpoint
+        let url = "http://18.219.112.140:8000/api/v1/create-group/"
+        
+        // Create group POST parameters
+        let params: [String : Any] = [
+            "name": nameField.text!,
+            "description": descriptionTextView.text!,
+            "dm": false
+        ]
+        
+        // HTTP Request
+        AF.request(url, method: .post, parameters: params, encoding: JSONEncoding.default).responseJSON { (response) in
+            switch response.result {
+                case .success(let value):
+                    if let data = value as? [String : Any] {
+                        if (data["status"] as! String == "success") {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
