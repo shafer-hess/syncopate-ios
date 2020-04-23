196919bc8e0ee4fe9f4e91fc9a9b4a69a87dadae
Emily ou <emilyou34@gmail.com>
2020-04-23 10:44:23 -0400

    Added view controller for add users and laid out UI

---
+++ Syncopate/AddUserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddUserViewController.swift
+//  Syncopate
+//
+//  Created by Emily Ou on 4/22/20.
+//  Copyright © 2020 Syncopate. All rights reserved.
+//
+
+import UIKit
+
+class AddUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
